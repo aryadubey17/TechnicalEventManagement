c1b23e88f7770a8f01423d451172e0cd533b18ce
aryadubey17 <aryadubey17@gmail.com>
2026-02-17 23:28:29 +0530

    Add files via upload

---
+++ src/main/java/com/tem/dao/DBConnection.java
@@ -0,0 +1,16 @@
+
+package com.tem.dao;
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tem","root","root");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
